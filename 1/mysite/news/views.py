7c16709c82c6a3387d18709dbf1ed7ddfeefbf1c
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-06-04 16:43:30 +0300

    Add files via upload

---
+++ 1/mysite/news/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News
+
+
+def index(request):
+    author = "allelleo"
+    news = News.objects.all()
+    context = {'news': news,
+               'author': author,
+               'title': 'Список Новостей'
+               }
+    return render(request=request, template_name='news/index.html', context=context)
